1ca53d7a441b46ab2f59318cfb7f8c351fbaff24
Daniel Lopes da Rocha <daniellopesdarocha1@gmail.com>
2016-03-05 09:14:40 -0300

    Modificando mensaagem de erro com a classe text-danger

--- produto-formulario.php
+++ produto-formulario.php
@@ -1,12 +1,20 @@
 <?php  include("cabecalho.php") ?>
 
-	<h1>Formulário de Cadastro</h1>
-	<form action="adiciona-produto.php">
-		Produto:
-		<input type="text" name="nome"><br/>
-		Preco:
-		<input type="number" name="preco"><br/>
-		<input type="submit" value="Cadastrar">
-	</form>
+<h1>Formulário de Cadastro</h1>
+<form action="adiciona-produto.php">
+	<table>
+		<tr>
+			<td>Produto</td>
+			<td><input class="form-control" type="text" name="nome"></td>
+		</tr>
+		<tr>
+			<td>Preco</td>
+			<td><input class="form-control" type="number" name="preco"></td>
+		</tr>
+		<tr>
+			<td><input class="btn btn-primary" type="submit" value="Cadastrar"></td>
+		</tr>
+	</table>
+</form>
 
 <?php include("rodape.php") ?>
